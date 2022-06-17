501b08a1e97790d980c1a19dff7ccdb7833052c3
Desmondsol <soltig66@gmail.com>
2022-06-17 22:59:57 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,37 @@
+
+import App from './App.vue'
+import './index.css'
+import {apolloClient} from './apollo-client'
+import {createRouter,createWebHistory} from 'vue-router'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import notfound from './screens/notfound.vue'
+import landing from './screens/landing.vue'
+
+
+
+const routes = [
+  { path: '/',
+    name: 'landing',
+   component: landing },
+  { path: '/about',
+    name: 'notfound',  
+    component: notfound },
+]
+ 
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+//export default router
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+ 
+  render: () => h(App),
+})
+
+app.use(router).mount('#app')
---
+++ src/screens/landing copy.vue
@@ -0,0 +1,21 @@
+<script setup>
+import headers from '../components/header.vue'
+import footers from '../components/footer.vue'
+import sidebar from '../components/sidebar.vue'
+import bodys from '../components/bodyComp/list_of_cont.vue'
+ 
+</script>
+<template>
+    <div class="flex min-h-screen"> 
+  <sidebar /> 
+
+   <div class="flex-1"> 
+     <headers /> 
+
+      <bodys />
+
+   </div>
+</div>
+  <footers />
+
+</template>
